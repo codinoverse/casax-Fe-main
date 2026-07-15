56bc540f4ac75a6395fb1d1662805b24b7229204
mnivedcodinoverse <nived.marumamula@codinoverse.com>
2026-07-15 11:13:14 +0530

    api integrated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -484,12 +484,6 @@ function Home({ isLoggedIn, onLogout }) {
                                 {property.totalAreaInSqFeet} sqft
                               </span>
                             )}
-                            {property.totalViews != null && (
-                              <span className="property-card-detail">
-                                <svg width="14" height="14" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle cx="12" cy="12" r="3"/></svg>
-                                {property.totalViews} Views
-                              </span>
-                            )}
                           </div>
                         </div>
                       </div>
